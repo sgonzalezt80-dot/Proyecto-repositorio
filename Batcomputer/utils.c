6186d33633eecd7080fc7f81e4f2185b71d535ae
Rom Ruiz <rominaruizm@icloud.com>
2026-04-28 23:25:48 -0600

    utils

---
+++ Batcomputer/utils.c
@@ -0,0 +1,19 @@
+// no modificar utilidades
+//filtro con manejo de errores y EOF 
+
+int get_val(char *msg) { 
+    /* Funcion para obtener un valor entero con manejo de errores estricto */
+    int v;
+    printf("%s", msg);
+    int res = scanf("%d", &v);
+    if (res != 1) {
+        if (res == EOF) {
+            clearerr(stdin); // Limpia el error si  EOF (Ctrl+D, Ctrl+Z)
+        } else {
+            int c;
+            while ((c = getchar()) != '\n' && c != EOF); // Limpia el buffer de entrada
+        }
+        return -1; 
+    }
+    return v;
+}
