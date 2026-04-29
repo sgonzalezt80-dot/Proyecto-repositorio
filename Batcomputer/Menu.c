aa3f47ba7fd1a289454180d14062ca9efa58a931
Rom Ruiz <rominaruizm@icloud.com>
2026-04-28 23:38:45 -0600

    haz el menu jeje

--- Batcomputer/Menu.c
+++ Batcomputer/Menu.c
@@ -2,32 +2,5 @@
 
 
 void menu(){
-    FILE *f; 
-    
-    f = fopen("users.txt", "a+");
-
-    char user[20] ={};
-    while(strcmp(user, "yawe")){
-        printf("Ingresa tu usuario\n");
-        scanf("%s", user);
-
-        fprintf(f, "%s\n", user);
-
-    }
-
-    while(0){
-
-        if(strcmp(user, "batman") == 0){
-            printf("Iron man apesta\n");
-            Batman();
-        }else{
-            if(strcmp(user, "GCPD") == 0){
-                printf("welcome\n");
-
-            }else{
-                printf("No le sabes");
-            }
-        }
-    }
-
-}
+    int opcion_principal;
+    struct User sesion_actual
