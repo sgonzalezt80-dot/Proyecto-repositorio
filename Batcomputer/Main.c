7ffa8970de0c2488d7126e859b75c39d94691f09
Rom Ruiz <rominaruizm@icloud.com>
2026-04-28 23:08:35 -0600

    menu pero aún sin includes específicos

--- Batcomputer/Main.c
+++ Batcomputer/Main.c
@@ -2,11 +2,72 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "Menu.c"
+#define MAX_INCIDENTS 10 
+enum boolean { False, True };
+enum role { ADMIN = 1, USER = 2 };
 
+//estrcutura de datos
+// Usuario para Login
+struct User {
+    char username[20];
+    char password[20];
+    enum role access_level;
+};
 
-int main(){
+// Base de Datos (Lista Doble)
+struct RecordData {
+    int id_registro;
+    char nombre[50];
+    char nivel_riesgo[20]; 
+};
 
-    menu(); 
+// Incidentes (Cola Simple)
+struct IncidentData {
+    int id_incidente;
+    char descripcion[100];
+    char prioridad[20];
+};
+
+// Auditoria (Pila LIFO)
+struct AuditData {
+    char operacion[100]; 
+};
+
+// Nodo Universal
+struct Node {
+    struct User user_info;
+    struct RecordData record_info;
+    struct IncidentData incident_info;
+    struct AuditData audit_info;
+    
+    struct Node *nxt;  
+    struct Node *prev; 
+};
+struct List {
+    struct Node *head;
+    struct Node *tail;
+    int count;
+};
+
+struct Queue {
+    struct Node *head;
+    struct Node *tail;
+    int count;
+};
+
+struct Stack {
+    struct Node *top;
+    int count;
+};
+
+#include "utils.c"
+// #include "login.c"  
+// #include "database.c"
+// #include "queue_incidents.c"
+// #include "stack_audit.c"
+
+void menu();
+int main() {
+    menu();
     return 0;
 }
