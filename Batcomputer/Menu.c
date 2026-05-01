03ff2059fd58d578e83490baaacaf85883f88857
Rom Ruiz <rominaruizm@icloud.com>
2026-04-30 22:23:51 -0600

    FINAL

--- Batcomputer/Menu.c
+++ Batcomputer/Menu.c
@@ -11,6 +11,8 @@ void menu(){
     struct IncidentData *h = NULL; 
     struct IncidentData *t = NULL; 
 
+    char audit_msg[100];
+
     // Carga la lista de crimenes [cola]
     if(!LoadCrimes(&h, &t)){
         printf("[!] Aviso: crimes.txt no encontrado o vacio. Iniciando cola limpia.\n");
@@ -39,13 +41,13 @@ void menu(){
             if (logueado == True) {
                 push_audit(&pila_auditoria, "Usuario inicio sesion en el sistema");
                 
-                // --- SUBMENU BATMAN ---
+                //  SUBMENU ADMIN 
                 if (sesion_actual.access_level == ADMIN) { 
                     int en_sesion = 1;
                         
                     while (en_sesion == 1) {
                         printf("\nAcceso de administrador concedido: BATCOMPUTADORA \n");
-                        printf("Bienvenido, %s\n", sesion_actual.username); 
+                        printf("Bienvenido(a), %s\n", sesion_actual.username); 
                         printf("1) Base de Datos Arkham\n"); 
                         printf("2) gestionar incidentes en Gotham\n"); 
                         printf("3) Auditoria del batsistema\n"); 
@@ -57,28 +59,32 @@ void menu(){
                             case 1:
                                 printf("\n|Cargando Base de Datos Arkham...|\n"); 
                                 printf("Que desea hacer\n");
-                                printf("1) Ver los registos\n2) Agregar un nuevo expediente\n3) Buscar un expediente\n4) Eliminar un expediente\n5) Salir\n");
+                                printf("1) Ver los registos\n2) Agregar un nuevo expediente\n3) Buscar un expediente\n4) Eliminar un expediente\n5) Regresar\n");
                                 opcion_arkham = get_val("Opcion: ");
 
                                 switch(opcion_arkham){
                                     case 1:
                                         VerRegistros(&hList);
-                                        push_audit(&pila_auditoria, "Batman consulto la base de datos de Arkham");
+                                        sprintf(audit_msg, "[%s] consulto la base de datos de Arkham", sesion_actual.username);
+                                        push_audit(&pila_auditoria, audit_msg);
                                         break;
                                     case 2:
                                         AgregarExpediente(&hList);
-                                        push_audit(&pila_auditoria, "Batman agrego un nuevo expediente a Arkham");
+                                        sprintf(audit_msg, "[%s] agrego un nuevo expediente a Arkham", sesion_actual.username);
+                                        push_audit(&pila_auditoria, audit_msg);
                                         break;
                                     case 3:
                                         exp_id = get_val("Escriba el id del expediente: ");
                                         if(!SearchExp(&hList, exp_id)){
                                             printf("Expediente no encontrado\n"); 
                                         }
-                                        push_audit(&pila_auditoria, "Batman busco un expediente especifico");
+                                        sprintf(audit_msg, "[%s] busco un expediente especifico", sesion_actual.username);
+                                        push_audit(&pila_auditoria, audit_msg);
                                         break;
                                     case 4:
                                         EliminarExpediente(&hList);
-                                        push_audit(&pila_auditoria, "Batman elimino un expediente clasificado");
+                                        sprintf(audit_msg, "[%s] elimino un expediente clasificado", sesion_actual.username);
+                                        push_audit(&pila_auditoria, audit_msg);
                                         break;
                                     case 5:
                                         break;
@@ -92,19 +98,22 @@ void menu(){
 
                                 if(opcion_quehacer == 1){
                                     ReportCrime(&h, &t);
-                                    push_audit(&pila_auditoria, "Batman reporto un incidente en la red");
+                                    sprintf(audit_msg, "[%s] reporto un incidente en la red", sesion_actual.username);
+                                    push_audit(&pila_auditoria, audit_msg);
                                 }else if(opcion_quehacer == 2){
                                     WatchCrimes(&h, &t); 
-                                    push_audit(&pila_auditoria, "Batman reviso los incidentes activos");
+                                    sprintf(audit_msg, "[%s] reviso los incidentes activos", sesion_actual.username);
+                                    push_audit(&pila_auditoria, audit_msg);
                                 }else if(opcion_quehacer == 3){
                                     SolveCrimes(&h, &t);
-                                    push_audit(&pila_auditoria, "Batman resolvio un incidente (Dequeue)");
+                                    sprintf(audit_msg, "[%s] resolvio un incidente", sesion_actual.username);
+                                    push_audit(&pila_auditoria, audit_msg);
                                 }
                                 break;
                                 
                             case 3:
-                                printf("\n|Cargando modulo de auditoria (Pila LIFO)...|\n");
-                                printf("1) Ver historial completo\n2) Revertir/Eliminar ultima accion (Pop)\n3) Regresar\n");
+                                printf("\n|Cargando modulo de auditoria...|\n");
+                                printf("1) Ver historial completo\n2) Revertir/Eliminar ultima accion \n3) Regresar\n");
                                 int opcion_auditoria = get_val("Opcion: ");
                                 
                                 if(opcion_auditoria == 1){
@@ -117,7 +126,8 @@ void menu(){
                             case 4:
                                 en_sesion = 0; 
                                 logueado = False;
-                                push_audit(&pila_auditoria, "Batman cerro sesion de la Batcomputadora");
+                                sprintf(audit_msg, "[%s] cerro sesion del sistema", sesion_actual.username);
+                                push_audit(&pila_auditoria, audit_msg);
                                 printf("\nCerrando sesion de Admin ...\n");
                                 break;
                                 
@@ -125,13 +135,13 @@ void menu(){
                                 printf("\n[!] Opcion invalida [!]\n");
                         }
                     }
-                } 
-                // --- SUBMENU POLICIA ---
+                }
+                // SUBMENU POLICIA 
                 else if (sesion_actual.access_level == USER) {
                     int en_sesion = 1;
                     while (en_sesion == 1) {
-                        printf("\nAcceso concedido: TERMINAL GCPD\n");
-                        printf("Bienvenido Oficial %s\n", sesion_actual.username);
+                        printf("\n=== Acceso concedido: TERMINAL GCPD ===\n");
+                        printf("Oficial en turno: %s\n", sesion_actual.username);
                         printf("1) Consultar Base de Datos Arkham\n");
                         printf("2) Reportar Incidente\n");
                         printf("3) Ver lista de incidentes\n"); 
@@ -143,20 +153,24 @@ void menu(){
                             case 1:
                                 printf("\n|Consultando base de datos...|\n");
                                 VerRegistros(&hList);
-                                push_audit(&pila_auditoria, "Oficial de policia consulto base de datos");
+                                sprintf(audit_msg, "[%s] consulto base de datos", sesion_actual.username);
+                                push_audit(&pila_auditoria, audit_msg);
                                 break;
                             case 2:
                                 ReportCrime(&h, &t);
-                                push_audit(&pila_auditoria, "Oficial de policia reporto un incidente");
+                                sprintf(audit_msg, "[%s] reporto un incidente", sesion_actual.username);
+                                push_audit(&pila_auditoria, audit_msg);
                                 break;
                             case 3:
                                 WatchCrimes(&h, &t);
-                                push_audit(&pila_auditoria, "Oficial de policia reviso incidentes");
+                                sprintf(audit_msg, "[%s] reviso incidentes", sesion_actual.username);
+                                push_audit(&pila_auditoria, audit_msg);
                                 break;
                             case 4:
                                 en_sesion = 0; 
                                 logueado = False;
-                                push_audit(&pila_auditoria, "Oficial de policia cerro sesion");
+                                sprintf(audit_msg, "[%s] cerro sesion", sesion_actual.username);
+                                push_audit(&pila_auditoria, audit_msg);
                                 printf("\nCerrando sesion de Usuario ...\n");
                                 break;
                             default:
